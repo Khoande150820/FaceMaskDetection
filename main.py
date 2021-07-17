b1e484208118da961cec17e2ead305a46c1f06a7
Khoande150820 <72441554+Khoande150820@users.noreply.github.com>
2021-07-17 10:13:27 +0700

    finishing

--- main.py
+++ main.py
@@ -1,47 +1,44 @@
 import cv2
 import numpy as np
 from tensorflow.keras import models
-
+from keras.models import load_model
 FRONTAL_FACES = 'haarcascades/haarcascade_frontalface_default.xml'
-
-model = models.load_model("model")
-path = "image/giroudDiscord.JPG"
-
-def result(img):
-    img = cv2.resize(img, (128, 128))
-    img = np.reshape(img, (1, 128, 128, 3)) / 255.0
-    if ((model.predict(img) > 0.5).astype("int32")) == 0:
-        return "Have Mask"
-    else:
-        return "No mask"
+face_model = cv2.CascadeClassifier(FRONTAL_FACES)
+model = load_model("VGG19-Face Mask Detection.h5")
+path = "image/__results___23_0.png"
 
 
-def detect_mask(img_path):
-    face_cascades = cv2.CascadeClassifier(FRONTAL_FACES)
-    cap = cv2.VideoCapture(0)
+mask_label = {0:'MASK',1:'NO MASK'}
+dist_label = {0:(0,255,0),1:(255,0,0)}
+
+
+def predict_image(image_dir):
     while True:
-        # ret, img = cap.read()
-        img = cv2.imread(img_path)
-        if img is None:
-            print("Can't find image")
-            return None
-        else:
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            faces = face_cascades.detectMultiScale(gray, 1.1, 3)
-            if faces is None:
-                print("No face detected!")
-                return 0
-            else:
-                for (x, y, w, h) in faces:
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                    face_detected = img[y:y + h, x:x + w]
-                    print(result(face_detected))
-                    cv2.putText(img, result(face_detected), (x, y - 1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-                cv2.imshow("img", img)
-                cv2.waitKey(0)
-                cv2.destroyAllWindows()
-                break
+        img = cv2.imread(image_dir)
+        img = cv2.cvtColor(img, cv2.IMREAD_GRAYSCALE)
+
+        # returns a list of (x,y,w,h) tuples
+        faces = face_model.detectMultiScale(img, scaleFactor=1.1, minNeighbors=4)
+
+        out_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
 
+        for i in range(len(faces)):
+            (x, y, w, h) = faces[i]
+            crop = out_img[y:y + h, x:x + w]
+            crop = cv2.resize(crop, (128, 128))
+            crop = np.reshape(crop, [1, 128, 128, 3]) / 255.0
+            mask_result = model.predict(crop).argmax()
+            cv2.rectangle(out_img, (x, y), (x + w, y + h), dist_label[mask_result], 1)
+            cv2.putText(
+                img, mask_label[mask_result],
+                (x, y - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
 
-detect_mask(path)
+        cv2.imshow('img', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        break
+
+
+predict_image(path)
